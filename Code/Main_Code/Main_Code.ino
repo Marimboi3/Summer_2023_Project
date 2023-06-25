e41e2188f512049b6da418467e5118215e8992c2
Marimboi3 <alexis59596@gmail.com>
2023-06-25 00:53:32 -0700

    Update Main_Code.ino

--- Code/Main_Code/Main_Code.ino
+++ Code/Main_Code/Main_Code.ino
@@ -1,38 +1,34 @@
 const int lightRead = 0;
-const int dataPin = 2;
-const int latchPin = 3;
-const int clockPin = 4;
-
-byte seven_seg_digits[10] = { B11111100,  // = 0
-                              B01100000,  // = 1
-                              B11011010,  // = 2
-                              B11110010,  // = 3
-                              B01100110,  // = 4
-                              B10110110,  // = 5
-                              B10111110,  // = 6
-                              B11100000,  // = 7
-                              B11111110,  // = 8
-                              B11100110   // = 9
-                             };
+const int center = 2;
+const int leftUp = 3;
+const int centerUp = 4;
+const int rightUp = 5;
+const int rightDown = 6;
+const int centerDown = 7;
+const int leftDown = 8;
 
 void setup() 
 {
-  pinMode(latchPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);
-  pinMode(clockPin, OUTPUT);
+  pinMode(center, OUTPUT);
+  pinMode(leftUp, OUTPUT);
+  pinMode(centerUp, OUTPUT);
+  pinMode(rightUp, OUTPUT);
+  pinMode(rightDown, OUTPUT);
+  pinMode(centerDown, OUTPUT);
+  pinMode(leftDown, OUTPUT);
   Serial.begin(9600);
 }
 
-void sevenSegWrite(byte digit)
-{
-  digitalWrite(latchPin, LOW);
-  shiftOut(dataPin, clockPin, LSBFIRST, seven_seg_digits[digit]);
-  digitalWrite(latchPin, HIGH);
-}
+
 
 void loop() 
 {
   int reading = analogRead(lightRead);
   Serial.println(reading);
+
+  if (reading >= 0 && reading < 5)
+  {
+    digitalWrite
+  }
   delay(1000);
 }
