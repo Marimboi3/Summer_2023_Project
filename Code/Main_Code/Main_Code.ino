b52f3cac13920d241ebf3d413c389a68c52d316a
Marimboi3 <alexis59596@gmail.com>
2023-06-25 22:37:48 -0700

    First Finished Code

--- Code/Main_Code/Main_Code.ino
+++ Code/Main_Code/Main_Code.ino
@@ -19,7 +19,115 @@ void setup()
   Serial.begin(9600);
 }
 
+void displayZero()
+{
+  digitalWrite(center, LOW);
+  digitalWrite(leftUp, HIGH);
+  digitalWrite(centerUp, HIGH);
+  digitalWrite(rightUp, HIGH);
+  digitalWrite(rightDown, HIGH);
+  digitalWrite(centerDown, HIGH);
+  digitalWrite(leftDown, HIGH);
+}
 
+void displayOne()
+{
+  digitalWrite(center, LOW);
+  digitalWrite(leftUp, LOW);
+  digitalWrite(centerUp, LOW);
+  digitalWrite(rightUp, HIGH);
+  digitalWrite(rightDown, HIGH);
+  digitalWrite(centerDown, LOW);
+  digitalWrite(leftDown, LOW);
+}
+
+void displayTwo()
+{
+  digitalWrite(center, HIGH);
+  digitalWrite(leftUp, LOW);
+  digitalWrite(centerUp, HIGH);
+  digitalWrite(rightUp, HIGH);
+  digitalWrite(rightDown, LOW);
+  digitalWrite(centerDown, HIGH);
+  digitalWrite(leftDown, HIGH);
+}
+
+void displayThree()
+{
+  digitalWrite(center, HIGH);
+  digitalWrite(leftUp, HIGH);
+  digitalWrite(centerUp, HIGH);
+  digitalWrite(rightUp, LOW);
+  digitalWrite(rightDown, LOW);
+  digitalWrite(centerDown, HIGH);
+  digitalWrite(leftDown, HIGH);
+}
+
+void displayFour()
+{
+  digitalWrite(center, HIGH);
+  digitalWrite(leftUp, HIGH);
+  digitalWrite(centerUp, LOW);
+  digitalWrite(rightUp, HIGH);
+  digitalWrite(rightDown, HIGH);
+  digitalWrite(centerDown, LOW);
+  digitalWrite(leftDown, LOW);
+}
+
+void displayFive()
+{
+  digitalWrite(center, HIGH);
+  digitalWrite(leftUp, HIGH);
+  digitalWrite(centerUp, HIGH);
+  digitalWrite(rightUp, LOW);
+  digitalWrite(rightDown, HIGH);
+  digitalWrite(centerDown, HIGH);
+  digitalWrite(leftDown, LOW);
+}
+
+void displaySix()
+{
+  digitalWrite(center, HIGH);
+  digitalWrite(leftUp, HIGH);
+  digitalWrite(centerUp, HIGH);
+  digitalWrite(rightUp, LOW);
+  digitalWrite(rightDown, HIGH);
+  digitalWrite(centerDown, HIGH);
+  digitalWrite(leftDown, LOW);
+}
+
+void displaySeven()
+{
+  digitalWrite(center, LOW);
+  digitalWrite(leftUp, LOW);
+  digitalWrite(centerUp, HIGH);
+  digitalWrite(rightUp, HIGH);
+  digitalWrite(rightDown, HIGH);
+  digitalWrite(centerDown, LOW);
+  digitalWrite(leftDown, LOW);
+}
+
+void displayEight()
+{
+  digitalWrite(center, HIGH);
+  digitalWrite(leftUp, HIGH);
+  digitalWrite(centerUp, HIGH);
+  digitalWrite(rightUp, HIGH);
+  digitalWrite(rightDown, HIGH);
+  digitalWrite(centerDown, HIGH);
+  digitalWrite(leftDown, HIGH);
+}
+
+void displayNine()
+{
+  digitalWrite(center, HIGH);
+  digitalWrite(leftUp, HIGH);
+  digitalWrite(centerUp, HIGH);
+  digitalWrite(rightUp, HIGH);
+  digitalWrite(rightDown, HIGH);
+  digitalWrite(centerDown, LOW);
+  digitalWrite(leftDown, LOW);
+}
 
 void loop() 
 {
@@ -28,7 +136,43 @@ void loop()
 
   if (reading >= 0 && reading < 5)
   {
-    digitalWrite
+    displayZero();
+  }
+  else if (reading <= 5 && reading < 10)
+  {
+    displayOne();
+  }
+  else if (reading <= 10 && reading < 15)
+  {
+    displayTwo();
+  }
+  else if (reading <= 15 && reading < 20)
+  {
+    displayThree
+  }
+  else if (reading <= 15 && reading < 20)
+  {
+    displayFour();
+  }
+  else if (reading <= 15 && reading < 20)
+  {
+    displayFive();
+  }
+  else if (reading <= 15 && reading < 20)
+  {
+    displaySix();
+  }
+  else if (reading <= 15 && reading < 20)
+  {
+    displaySeven();
+  }
+  else if (reading <= 15 && reading < 20)
+  {
+    displayEight();
+  }
+  else if (reading <= 15 && reading < 20)
+  {
+    displayNine();
   }
   delay(1000);
 }
